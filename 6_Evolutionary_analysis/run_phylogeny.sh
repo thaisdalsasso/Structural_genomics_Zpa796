01b807c5d0acc5242a0fcad79f5df9f4f0b0938f
Thaís Dal'Sasso <thaisdalsasso@gmail.com>
2026-07-23 12:06:09 +0200

    Update

---
+++ 6_Evolutionary_analysis/run_phylogeny.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+root_dir="./phylogeny_homologs"
+multifasta_input="${root_dir}/G.12/G.12_homologs.fasta"
+outdir="${root_dir}/phylogeny/G.12"
+
+mkdir -p "${outdir}"
+
+alignment="${outdir}/G.12_all_proteins_mafft_linsi.fasta"
+trimmed_alignment="${outdir}/G.12_all_homologs_mafft_linsi_trimal_gt0.9_cons60_w3.fasta"
+iqtree_prefix="${outdir}/G.12_all_homologs_iqtree"
+
+
+# Align sequences
+mafft --localpair --maxiterate 1000 "${multifasta_input}" > "${alignment}"
+
+# Trim alignment
+trimal -in "${alignment}" -out "${trimmed_alignment}" -gt 0.9 -cons 60 -w 3
+
+# Run ML phylogeny
+iqtree -s "${trimmed_alignment}" -st AA --runs 10 --ufboot 1000 -m TEST -pre "${iqtree_prefix}"
+
+echo "Done!"
