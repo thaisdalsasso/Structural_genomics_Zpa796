e6e5932b335a1e149ad24e9cbe15a9b6f3ea5711
Thaís Dal'Sasso <thaisdalsasso@gmail.com>
2026-07-28 10:47:29 +0200

    Update

--- 6_Evolutionary_analysis/run_hyphy_workflow.sh
+++ 6_Evolutionary_analysis/run_hyphy_workflow.sh
@@ -1,45 +1,45 @@
 #!/usr/bin/env bash
 
-ROOT="/path2/phylogeny_homologs"
-WORKDIR="${ROOT}/Zpa796_copies"
+root_dir="/path2/phylogeny_homologs"
+work_dir="${root_dir}/Zpa796_copies"
 
 
 # Prepare data input for HyPhy
-python3.9 "${ROOT}/prepare_hyphy_inputs_Zpa796.py"
+python3.9 "${root_dir}/prepare_hyphy_inputs_Zpa796.py"
 
 # Run phylogeny and selection models
-for CLUSTER in "G.04" "G.12"; do
-  CLUSTER_DIR="${WORKDIR}/${CLUSTER}"
-  ALIGNMENT="${CLUSTER_DIR}/${CLUSTER}_Zpa796_mature_codon_alignment_trimmed_gappyout.fasta"
-  TREE_PREFIX="${CLUSTER_DIR}/${CLUSTER}_Zpa796_mature_codon_iqtree"
+for cluster in "G.04" "G.12"; do
+  cluster_dir="${work_dir}/${cluster}"
+  alignment="${cluster_dir}/${cluster}_Zpa796_mature_codon_alignment_trimmed_gappyout.fasta"
+  tree_prefix="${cluster_dir}/${cluster}_Zpa796_mature_codon_iqtree"
 
-  echo "Inferring nucleotide ML tree for ${CLUSTER}"
+  echo "Inferring nucleotide ML tree for ${cluster}"
   iqtree2 \
-    -s "${ALIGNMENT}" \
+    -s "${alignment}" \
     -st DNA \
     -m TEST \
     --runs 10 \
-    -pre "${TREE_PREFIX}"
+    -pre "${tree_prefix}"
 
-  echo "Running HyPhy MEME for ${CLUSTER}"
-  mkdir -p "${CLUSTER_DIR}/hyphy_MEME"
+  echo "Running HyPhy MEME for ${cluster}"
+  mkdir -p "${cluster_dir}/hyphy_MEME"
   hyphy meme \
-    --alignment "${ALIGNMENT}" \
-    --tree "${TREE_PREFIX}.treefile" \
+    --alignment "${alignment}" \
+    --tree "${tree_prefix}.treefile" \
     --branches All \
     --pvalue 0.05 \
-    --output "${CLUSTER_DIR}/hyphy_MEME/${CLUSTER}_Zpa796_MEME.json" \
-    2>&1 | tee "${CLUSTER_DIR}/hyphy_MEME/${CLUSTER}_Zpa796_MEME.log"
+    --output "${cluster_dir}/hyphy_MEME/${cluster}_Zpa796_MEME.json" \
+    2>&1 | tee "${cluster_dir}/hyphy_MEME/${cluster}_Zpa796_MEME.log"
 
-  echo "Running HyPhy FEL for ${CLUSTER}"
-  mkdir -p "${CLUSTER_DIR}/hyphy_FEL"
+  echo "Running HyPhy FEL for ${cluster}"
+  mkdir -p "${cluster_dir}/hyphy_FEL"
   hyphy fel \
-    --alignment "${ALIGNMENT}" \
-    --tree "${TREE_PREFIX}.treefile" \
+    --alignment "${alignment}" \
+    --tree "${tree_prefix}.treefile" \
     --branches All \
     --pvalue 0.05 \
-    --output "${CLUSTER_DIR}/hyphy_FEL/${CLUSTER}_Zpa796_FEL.json" \
-    2>&1 | tee "${CLUSTER_DIR}/hyphy_FEL/${CLUSTER}_Zpa796_FEL.log"
+    --output "${cluster_dir}/hyphy_FEL/${cluster}_Zpa796_FEL.json" \
+    2>&1 | tee "${cluster_dir}/hyphy_FEL/${cluster}_Zpa796_FEL.log"
 done
 
 echo "Done!"
